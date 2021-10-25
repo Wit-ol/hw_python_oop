930d26b4786f81d0be51b46c66e0ee89db61115a
Олеся Митрофанова <mit.ols@yandex.ru>
2021-10-26 02:17:53 +0300

    My new commit

--- homework.py
+++ homework.py
@@ -4,7 +4,8 @@ from typing import List
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
-    def __init__(self, training_type: str, duration: float, distance: float, speed: float, calories: float) -> None:
+    def __init__(self, training_type: str, duration: float, distance: float,
+                 speed: float, calories: float) -> None:
         self.training_type = training_type
         self.duration = duration
         self.distance = distance
@@ -12,11 +13,11 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {format(self.duration, ".3f")} ч.; '
-                f'Дистанция: {format(self.distance, ".3f")} км; '
-                f'Ср. скорость: {format(self.speed, ".3f")} км/ч; '
-                f'Потрачено ккал: {format(self.calories, ".3f")}.')
+        return(f'Тип тренировки: {self.training_type}; '
+               f'Длительность: {format(self.duration, ".3f")} ч.; '
+               f'Дистанция: {format(self.distance, ".3f")} км; '
+               f'Ср. скорость: {format(self.speed, ".3f")} км/ч; '
+               f'Потрачено ккал: {format(self.calories, ".3f")}.')
 
 
 class Training:
@@ -24,7 +25,8 @@ class Training:
     M_IN_KM: int = 1000
     LEN_STEP: float = 0.65
 
-    def __init__(self, action: int, duration: float, weight: float) -> None:
+    def __init__(self, action: int, duration: float,
+                 weight: float) -> None:
         self.action = action
         self.duration = duration
         self.weight = weight
@@ -43,7 +45,9 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.__class__.__name__, self.duration, self.get_distance(),
+        return InfoMessage(self.__class__.__name__,
+                           self.duration,
+                           self.get_distance(),
                            self.get_mean_speed(), self.get_spent_calories())
 
 
@@ -53,8 +57,8 @@ class Running(Training):
     COEFF_CALORIE_2: int = 20
 
     def get_spent_calories(self) -> float:
-        return (self.COEFF_CALORIE_1 * self.get_mean_speed() - self.COEFF_CALORIE_2) \
-               * self.weight / self.M_IN_KM * self.duration * 60
+        return (self.COEFF_CALORIE_1 * self.get_mean_speed()
+                - self.COEFF_CALORIE_2) * self.weight / self.M_IN_KM * self.duration * 60
 
 
 class SportsWalking(Training):
@@ -62,13 +66,16 @@ class SportsWalking(Training):
     COEFF_CALORIE_1: float = 0.035
     COEFF_CALORIE_2: float = 0.029
 
-    def __init__(self, action: int, duration: float, weight: float, height: float) -> None:
+    def __init__(self, action: int, duration: float,
+                 weight: float, height: float) -> None:
         super().__init__(action, duration, weight)
         self.height = height
 
     def get_spent_calories(self) -> float:
-        return (self.COEFF_CALORIE_1 * self.weight + (self.get_mean_speed()**2 // self.height) * self.COEFF_CALORIE_2
-                * self.weight) * self.duration * 60
+        return(self.COEFF_CALORIE_1 * self.weight + (self.get_mean_speed()**2
+               // self.height)
+               * self.COEFF_CALORIE_2
+               * self.weight) * self.duration * 60
 
 
 class Swimming(Training):
@@ -77,7 +84,8 @@ class Swimming(Training):
     COEFF_CALORIE_1: float = 1.1
     COEFF_CALORIE_2: float = 2
 
-    def __init__(self, action: int, duration: float, weight: float, length_pool: float, count_pool: float) -> None:
+    def __init__(self, action: int, duration: float,
+                 weight: float, length_pool: float, count_pool: float) -> None:
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
