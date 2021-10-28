891702ec9ab3e359f6c193155aab2542063f4f5e
Олеся Митрофанова <mit.ols@yandex.ru>
2021-10-28 17:51:59 +0300

    review 2.0

--- homework.py
+++ homework.py
@@ -1,16 +1,15 @@
 from typing import List
+from dataclasses import dataclass
 
 
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-# тут можно применить декоратор, возможно, но декораторы еще только будут
-    def __init__(self, training_type: str, duration: float, distance: float,
-                 speed: float, calories: int) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: int
 
     def get_message(self) -> str:
         return(f'Тип тренировки: {self.training_type}; '
@@ -53,19 +52,19 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    COEFF_CALORIE_1: int = 18
-    COEFF_CALORIE_2: int = 20
+    COEFF_RUN_1: int = 18
+    COEFF_RUN_2: int = 20
 
     def get_spent_calories(self) -> float:
-        return ((self.COEFF_CALORIE_1 * self.get_mean_speed()
-                - self.COEFF_CALORIE_2) * self.weight
+        return ((self.COEFF_RUN_1 * self.get_mean_speed()
+                - self.COEFF_RUN_2) * self.weight
                 / self.M_IN_KM * self.duration * 60)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    COEFF_CALORIE_1: float = 0.035
-    COEFF_CALORIE_2: float = 0.029
+    COEFF_WALK_1: float = 0.035
+    COEFF_WALK_2: float = 0.029
 
     def __init__(self, action: int, duration: float,
                  weight: float, height: float) -> None:
@@ -73,17 +72,17 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        return ((self.COEFF_CALORIE_1 * self.weight + (self.get_mean_speed()**2
+        return ((self.COEFF_WALK_1 * self.weight + (self.get_mean_speed()**2
                 // self.height)
-                * self.COEFF_CALORIE_2
+                * self.COEFF_WALK_2
                 * self.weight) * self.duration * 60)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP: float = 1.38
-    COEFF_CALORIE_1: float = 1.1
-    COEFF_CALORIE_2: float = 2
+    COEFF_SWIM_1: float = 1.1
+    COEFF_SWIM_2: float = 2
 
     def __init__(self, action: int, duration: float,
                  weight: float, length_pool: float, count_pool: float) -> None:
@@ -96,18 +95,21 @@ class Swimming(Training):
                 / self.duration)
 
     def get_spent_calories(self) -> float:
-        return ((self.get_mean_speed() + self.COEFF_CALORIE_1)
-                * self.COEFF_CALORIE_2 * self.weight)
+        return ((self.get_mean_speed() + self.COEFF_SWIM_1)
+                * self.COEFF_SWIM_2 * self.weight)
 
 
 def read_package(workout_type: str, data: List) -> Training:
     """Прочитать данные полученные от датчиков."""
-    workout = {
+    WORKOUT = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
     }
-    return workout[workout_type](*data)
+    if workout_type in WORKOUT:
+        return WORKOUT[workout_type](*data)
+    else:
+        raise KeyError('Unknown training code')
 
 
 def main(training: Training) -> None:
